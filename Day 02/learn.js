e1221d2d4688bbdb6b8f0d960683cfcb8fa7b149
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2025-04-03 22:11:28 +0530

    Day 02

--- Day 02/learn.js
+++ Day 02/learn.js
@@ -1,3 +1,6 @@
+// with Statement (Rarely Used)
+// Allows shorthand access to object properties but is discouraged due to scope confusion.
+
 const person = { name: "Dhruvik", age: 25 };
 with (person) {
     console.log(name); // "Dhruvik"
