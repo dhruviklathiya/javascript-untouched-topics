032ed1274e75a652bb1ba389e9a888653e7d4913
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2025-04-15 23:50:31 +0530

    Day 14

---
+++ Day 14/learn.js
@@ -0,0 +1,11 @@
+// import.meta (Module Metadata)
+
+// Gives details about the module itself.
+
+console.log(import.meta.url);
+
+
+// 🧠 What is import.meta?
+// In JavaScript ES modules, import.meta is an object that contains metadata about the current module — basically, information that helps you understand or interact with the module at runtime.
+
+// It's not available in CommonJS (like Node.js require() style by default) — it's specific to ES Modules (ESM).
